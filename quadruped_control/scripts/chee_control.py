b436120e37f63d04fc4a8c6749e50de6cd7a45d4
sevocrear <sevocrear@gmail.com>
2021-03-11 14:35:07 +0300

    updated all

--- quadruped_control/scripts/chee_control.py
+++ quadruped_control/scripts/chee_control.py
@@ -4,6 +4,7 @@ import rospy
 import time
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
+from gazebo_msgs.msg import LinkStates
 class cheetah_control():
     def __init__(self, type_of_control = 'position', time_pause_before_control = 0, rate_value = 1000):
         rospy.init_node('cheetah_control', anonymous=True)
@@ -15,17 +16,18 @@ class cheetah_control():
                     'FL_thigh' : ['left_forward_leg_hip'], 
                      'FL_calf' : ['left_forward_hip_calf'],
 
-                       'FR_hip' : ['right_forward_motor_leg'], 
-                     'FR_thigh' : ['right_forward_leg_hip'], 
-                      'FR_calf' : ['right_forward_hip_calf'],
+                    #    'FR_hip' : ['right_forward_motor_leg'], 
+                    #  'FR_thigh' : ['right_forward_leg_hip'], 
+                    #   'FR_calf' : ['right_forward_hip_calf'],
 
                        'RL_hip': ['left_back_motor_leg'],
                      'RL_thigh': ['left_back_leg_hip'], 
                       'RL_calf': ['left_back_hip_calf'],
 
-                       'RR_hip' : ['right_back_motor_leg'], 
-                     'RR_thigh' : ['right_back_leg_hip'], 
-                       'RR_calf': ['right_back_hip_calf']}
+                    #    'RR_hip' : ['right_back_motor_leg'], 
+                    #  'RR_thigh' : ['right_back_leg_hip'], 
+                    #    'RR_calf': ['right_back_hip_calf']
+                    }
 
         if type_of_control == 'position':
             self.controllers = list(map(lambda x: x + '_position_controller', self.joints))
@@ -40,26 +42,18 @@ class cheetah_control():
             i += 1
 
         self.cheetah_joints_sub = rospy.Subscriber("/quadruped/joint_states", JointState, self.joints_pos_callback)
+        self.cheetah_links_sub = rospy.Subscriber("/gazebo/link_states", LinkStates, self.body_pos_callback)
+
         self.rate = rospy.Rate(rate_value)
         self.joints_positions = {}
         self.joints_velocities = {}
+
+        self.body_position = {}
+        self.body_orientation = {}
+        self.body_twist_angular = {}
+        self.body_twist_linear = {}
+
     def move_joint(self,joint, pos):
-        """
-        i - number of joint: 
-        1: 'FL_hip_controller', 
-        2: 'FL_thigh_controller', 
-        3: 'FL_calf_controller',
-        4: 'FR_hip_controller', 
-        5: 'FR_thigh_controller', 
-        6: 'FR_calf_controller',
-        7: 'RL_hip_controller', 
-        8: 'RL_thigh_controller', 
-        9: 'RL_calf_controller',
-        10: 'RR_hip_controller', 
-        11: 'RR_thigh_controller', 
-        12: 'RR_calf_controller'
-        pos - angle in rad
-        """	
         self.pub[joint].publish(pos)	
 
     def joint_name(self, joint):
@@ -70,10 +64,27 @@ class cheetah_control():
     def joints_pos_callback(self, cheetah_joints_pos):
         '''
         joints:
-        ['FL_calf_joint', 'FL_hip_joint', 'FL_thigh_joint', 
-        'FR_calf_joint', 'FR_hip_joint', 'FR_thigh_joint', 
-        'RL_calf_joint', 'RL_hip_joint', 'RL_thigh_joint', 
-        'RR_calf_joint', 'RR_hip_joint', 'RR_thigh_joint']
+        ['FL_calf', 'FL_hip', 'FL_thigh', 
+        'FR_calf', 'FR_hip', 'FR_thigh', 
+        'RL_calf', 'RL_hip', 'RL_thigh', 
+        'RR_calf', 'RR_hip', 'RR_thigh']
         '''
         self.joints_positions = dict(zip(cheetah_joints_pos.name, cheetah_joints_pos.position))
         self.joints_velocities = dict(zip(cheetah_joints_pos.name, cheetah_joints_pos.velocity))
+
+    #callback function for robot pos
+    def body_pos_callback(self, cheetah_links_pos):
+        '''
+        links:
+
+        '''
+        cheetah_links_pos.name
+        link_poses = dict(zip(cheetah_links_pos.name, cheetah_links_pos.pose))
+        link_twists = dict(zip(cheetah_links_pos.name, cheetah_links_pos.twist))
+
+        self.body_position = link_poses['quadruped_robot::body'].position
+        self.body_orientation = link_poses['quadruped_robot::body'].orientation
+        self.body_twist_angular = link_twists['quadruped_robot::body'].angular
+        self.body_twist_linear = link_twists['quadruped_robot::body'].linear
+    def forward_kinematics(self, ):
+        pass
--- quadruped_control/scripts/control_main.py
+++ quadruped_control/scripts/control_main.py
@@ -3,9 +3,13 @@
 import rospy
 from chee_control import cheetah_control
 import numpy as np
+import sys, os
 
 if __name__ == '__main__':
+	# print(os.path.realpath(__file__))
 	cheetah_control_pos = cheetah_control(type_of_control = 'torque')
+	# chain = kp.build_chain_from_urdf(open(os.path.join(os.path.dirname(sys.argv[0]) ,os.path.relpath("../../quadruped_robot/urdf/quadruped_robot.urdf"))).read().encode())
+
 	goal = [0.0,0.0,0.0]*4
 	K = [50, 50,10]
 	d = [2, 2, 0.1]
@@ -14,7 +18,7 @@ if __name__ == '__main__':
 		try:
 			if not cheetah_control_pos.joints_positions:
 				continue
-			for i in range(0,12,3):
+			for i in range(0,6,3):
 				idx = 0+i,1+i,2+i
 				torque_0 = K[0]*(goal[idx[0]]-cheetah_control_pos.joints_positions[cheetah_control_pos.joints[joints[idx[0]]][0]]) + d[0]*(0-cheetah_control_pos.joints_velocities[cheetah_control_pos.joints[joints[idx[0]]][0]])
 
@@ -25,6 +29,9 @@ if __name__ == '__main__':
 				cheetah_control_pos.move_joint(joints[idx[0]], torque_0)
 				cheetah_control_pos.move_joint(joints[idx[1]], torque_1)
 				cheetah_control_pos.move_joint(joints[idx[2]], torque_2)
+			# print(cheetah_control_pos.body_position)
+			# print(cheetah_control_pos.joints_positions)
+			
 			cheetah_control_pos.rate.sleep()
 		except rospy.ROSInterruptException:
 			pass
